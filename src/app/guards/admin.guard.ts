2f398ec8cee30f6f9b5c72663a803b8df091781d
omerlaleee <omerlaleee@gmail.com>
2023-06-18 00:21:26 +0300

    Some styles were changed.

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -13,15 +13,9 @@ export class AdminGuard implements CanActivate {
 
   constructor(private authService: AuthService, private userService: UserService, private toastrService: ToastrService, private router: Router) { }
 
-
-  // userIsAdmin: boolean = false;
-  // flag: boolean = false;
-
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    // window.localStorage.getItem("isAdmin")
 
     if( window.localStorage.getItem("isAdmin")=="true"){
       return true;
@@ -31,24 +25,6 @@ export class AdminGuard implements CanActivate {
       this.toastrService.info("Bu Sayfaya Erişim Hakkınız Yok!");
       return false;
     }
-
-    // this.userService.isAdmin(this.authService.loggedInUser.id).subscribe(response => {
-    //   this.userIsAdmin = response.success;
-    //   if (this.userIsAdmin) {
-    //     console.log(this.userIsAdmin);
-    //     this.flag = true;
-    //   }
-    //   else {
-    //     this.flag = false;
-    //   }
-    // })
-    // //console.log(userIsAdmin);
-    // if (!this.flag) {
-    //   this.router.navigate(["/"]);
-    //   this.toastrService.info("Bu Sayfaya Erişim Hakkınız Yok!");
-    // }
-    // return this.flag;
-
   }
 
 }
